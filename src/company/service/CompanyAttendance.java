bc80365e46abd9ceb3382e6f1b2e2f3296f961c9
KimSeongChan97 <sck2270@gmail.com>
2024-07-30 14:38:31 +0900

    Add files via upload

--- src/company/service/CompanyAttendance.java
+++ src/company/service/CompanyAttendance.java
@@ -1,10 +1,29 @@
 package company.service;
-//출결체크
+
+import java.util.Scanner;
+
+import company.dao.CompanyDAO;
+
 public class CompanyAttendance implements Company {
 
-	@Override
-	public void execute() {
-		
-	}
+    @Override
+    public void execute() {
+        Scanner sc = new Scanner(System.in);
+        
+        System.out.print("출석 ID 입력: ");
+        String attendanceId = sc.nextLine();
+        System.out.print("회사 ID 입력: ");
+        String companyId = sc.nextLine();
+        System.out.print("날짜 입력 (yyyy-mm-dd): ");
+        String date = sc.nextLine();
+        System.out.print("출석 여부 입력 (true/false): ");
+        boolean isPresent = sc.nextBoolean();
+        
+        
+        CompanyDAO companyDAO = CompanyDAO.getInstance();
+        
+        companyDAO.addAttendance(attendanceId, companyId, date, isPresent);
+ 
+    }
 
 }
